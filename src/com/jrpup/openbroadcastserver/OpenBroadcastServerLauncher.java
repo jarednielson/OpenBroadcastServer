65c5652e61e3f17479120bafbc92beeae0967c73
jarednielson <jarednielsonn@gmail.com>
2017-08-06 18:35:11 -0600

    Add command line argument for port number

--- src/com/jrpup/openbroadcastserver/OpenBroadcastServerLauncher.java
+++ src/com/jrpup/openbroadcastserver/OpenBroadcastServerLauncher.java
@@ -8,19 +8,30 @@ public class OpenBroadcastServerLauncher {
 	private static int port = 60000;
 	
 	public static void main(String[] args) {
+		if(args.length > 1){
+			try{
+				port = Integer.parseInt(args[0]);
+			} catch(NumberFormatException e){
+				System.out.println("Could not parse Port number");
+				return;
+			}
+		}
+		
+		if(port < 0 ){
+			System.out.println("Invalid Port number. Port must be greater than or equal to 0");
+			return;
+		}
+		
 		ConnectionListener conListener = ConnectionListener.getConnectionManager(port);
 		
 		
 		ConnectionManager conManager = new ConnectionManager();
 		conListener.beginListen(conManager);
-		
-		
-		
+			
 		Scanner in = new Scanner(System.in);
 		
 		in.nextLine();
-		
-		
+				
 		conListener.endListen();
 		try {
 			conManager.shutDown();
